ec0f98acf079e9523d06dccf6c7be1d3274e86b1
James Meyer <jmeyer@boundlessgeo.com>
2018-04-09 13:37:14 -0400

    Model allows null as a file, update resource to respect that.

--- osgeo_importer/api.py
+++ osgeo_importer/api.py
@@ -38,8 +38,8 @@ class UploadedLayerResource(ModelResource):
     configuration_options = DictField(attribute='configuration_options', null=True)
     fields = ListField(attribute='fields')
     status = CharField(attribute='status', readonly=True, null=True)
-    file_type = CharField(attribute='file_type', readonly=True)
-    file_name = CharField(attribute='file_name', readonly=True)
+    file_type = CharField(attribute='file_type', readonly=True, null=True)
+    file_name = CharField(attribute='file_name', readonly=True, null=True)
     layer_name = CharField(attribute='layer_name', readonly=True)
 
     class Meta:
